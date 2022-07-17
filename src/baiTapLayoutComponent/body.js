940ab8f42ec5b8441992efefc1aeef058f4affe3
unknown <thanhtien18399>
2022-07-18 00:07:11 +0700

    react-b1

---
+++ src/baiTapLayoutComponent/body.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Banner from './banner'
+import Item from './item'
+export class body extends Component {
+  render() {
+    return (
+      <div >
+            <Banner/>
+            <Item/>
+      </div>
+    )
+  }
+}
+
+export default body
